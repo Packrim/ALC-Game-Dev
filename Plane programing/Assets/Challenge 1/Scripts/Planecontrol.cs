12a765a0453859e42146e8bc9465a34a87ece2ab
Cameron Scholl <Packrim@outlook.com>
2020-09-25 12:42:20 -0600

    Plane prototype

---
+++ Plane programing/Assets/Challenge 1/Scripts/Planecontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planecontrol : MonoBehaviour
+{
+//access modifier type name operator value
+  
+   
+   
+  
+   public float forwardInput;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //move vehical forward
+        transform.Translate(Vector3.forward);
+   
+    }
+}
