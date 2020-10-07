6e3d6fa865b79d802d1ab2e817e1a0374a3c0fc2
Cameron Scholl <Packrim@outlook.com>
2020-10-07 12:43:39 -0600

    Animal move forward attempted to shoot food projectile

---
+++ prototype 2 animal stanmpede/Assets/Scripts/Playercontrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontrol : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 19.0f;
+    
+    public GameObject foodProjectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement for character left and right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        //constrains players movement to the left
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //contains players movement to the right
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(foodProjectile, transform.position, foodProjectiled.transform.rotation);
+        }
+    }
+}
