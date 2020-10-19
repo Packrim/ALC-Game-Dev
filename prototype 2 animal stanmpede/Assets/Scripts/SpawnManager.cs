a1e0ccdb66a44da10ef1a40620aa17fd620480f7
Cameron Scholl <Packrim@outlook.com>
2020-10-19 12:39:14 -0600

    Finished up the Animal game

--- prototype 2 animal stanmpede/Assets/Scripts/SpawnManager.cs
+++ prototype 2 animal stanmpede/Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    //public int animalIndex;
+   private float spawnRangeX = 20f;
+    private float spawnPosZ = 29.5f;
     
+    private float startDelay = 2f;
+    private float spawnInterval= 1.5f;
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
     // Update is called once per frame
     void Update()
     {
+    
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
         
-      if(Input.GetKeyDown(KeyCode.S))
-      {
-            int animalIndex = Random.Range(0, animalPrefabs.length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-      }
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
---
+++ prototype 2 animal stanmpede/Assets/Scripts/detectCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectCollision : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider other)
+    {
+    //Destroy this object
+        Destroy(gameObject);
+    //Destroy the other object it collides with
+        Destroy(other.gameObject);
+    }
+}
