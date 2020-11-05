a3f1623d8eb361b8ca99ce845011a1ad6428af85
Cameron Scholl <Packrim@outlook.com>
2020-11-05 12:45:36 -0700

    Project 3 started

---
+++ New Unity Project (1)/Assets/scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    
+    public bool isOnGround = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
