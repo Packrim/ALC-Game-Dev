462787bdd7a2a692cf6f9577f4573404dda5e418
Cameron Scholl <Packrim@outlook.com>
2020-12-04 08:13:35 -0700

    Personal Project stuff

---
+++ Personal project/Assets/Scripts/playerControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    private float forwardInput;
+    private float turnInput;
+    private float speed = 10; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnInput = Input.GetAxis("Horizontal");
+        forwardInput = forwardInput.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
+        tramsform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * speed);
+    }
+}
