ce04e328faefa03be090f301ea4c8cf906e3d6e6
Cameron Scholl <Packrim@outlook.com>
2020-12-08 10:13:39 -0700

    worked on personal project

--- Personal project/Assets/Scripts/playerControl.cs
+++ Personal project/Assets/Scripts/playerControl.cs
@@ -7,7 +7,7 @@ public class playerControl : MonoBehaviour
     private float forwardInput;
     private float turnInput;
     private float speed = 15; 
-    private float turnSpeed = 100;
+    private float turnSpeed = 150;
 
     // Start is called before the first frame update
     void Start()
